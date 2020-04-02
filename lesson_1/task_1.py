7fb1d3745b84325937fe6f1fe0a5b23765727849
Artur Oganesyan <artur1948.rf@mail.ru>
2020-04-03 02:29:49 +0300

    completed lesson_1

---
+++ lesson_1/task_1.py
@@ -0,0 +1,17 @@
+a = 'String'
+b = 1
+c = 2.0
+d = True
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+print('Your name is', name)
+print('Your age is', age)
+print('I am Captain Obvious =)')
---
+++ lesson_1/task_3.py
@@ -0,0 +1,3 @@
+n = input('Enter number: ')
+triple_sum = int(n) + int(n * 2) + int(n * 3)
+print(triple_sum)
